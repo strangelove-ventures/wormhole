3e724bedc2b233423bf04952db11bbf905a4f9c9
Kaku <105181329+kakucodes@users.noreply.github.com>
2024-09-18 11:53:29 -0500

    fixed spelling errors in 2 files

--- scripts/sort-cspell-custom-words.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This script sorts the custom words in cspell-custom-words.txt alphabetically
-# and includes duplicate words.
-#
-# Note: Run this script from the root of the project.
-sort cspell-custom-words.txt -o cspell-custom-words.txt -f
